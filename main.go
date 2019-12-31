5c6b676aa6d43e2792987888470799aa2f06c4b6
stone <335612970@qq.com>
2019-12-31 11:16:39 +0800

    Gin实践：搭建Blog API's（一）

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/stone955/my-gin-blog/pkg/setting"
 	"github.com/stone955/my-gin-blog/router"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
 	r := router.Register()
-	s := &http.Server{
+	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", setting.HTTPPort),
 		Handler:      r,
 		ReadTimeout:  setting.ReadTimeout,
 		WriteTimeout: setting.WriteTimeout,
 	}
-	s.ListenAndServe()
+
+	var wg sync.WaitGroup
+	// 优雅关闭
+	exit := make(chan os.Signal)
+	//监听 Ctrl+C 信号
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-exit
+		wg.Add(1)
+		defer wg.Done()
+		//使用context控制srv.Shutdown的超时时间
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatalf("Fatal to shutdown: %v\n", err)
+		}
+	}()
+
+	// 启动 server
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Fatal to listen and serve: %v\n", err)
+	}
+
+	wg.Wait()
+
+	log.Println("Gracefully shutdown the http server...")
 }
