6c43bb3f4fec274034b3bad2f294b22de7f39774
stone <335612970@qq.com>
2020-01-09 14:13:03 +0800

    Gin实践：搭建Blog API's（六） 使用endless库实现优雅停机

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ replace (
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-ini/ini v1.51.1
 	github.com/jinzhu/gorm v1.9.11
--- main.go
+++ main.go
@@ -1,51 +1,65 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"github.com/fvbock/endless"
 	"github.com/stone955/my-gin-blog/pkg/setting"
 	"github.com/stone955/my-gin-blog/router"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"sync"
 	"syscall"
-	"time"
 )
 
 func main() {
 	r := router.Register()
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:      r,
-		ReadTimeout:  setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-	}
 
-	var wg sync.WaitGroup
-	// 优雅关闭
-	exit := make(chan os.Signal)
-	//监听 Ctrl+C 信号
-	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
-	go func() {
-		<-exit
-		wg.Add(1)
-		defer wg.Done()
-		//使用context控制srv.Shutdown的超时时间
-		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(ctx); err != nil {
-			log.Fatalf("Fatal to shutdown: %v\n", err)
+	/*
+		// 优雅停机：自己实现
+		srv := &http.Server{
+			Addr:         fmt.Sprintf(":%d", setting.HTTPPort),
+			Handler:      r,
+			ReadTimeout:  setting.ReadTimeout,
+			WriteTimeout: setting.WriteTimeout,
 		}
-	}()
 
-	// 启动 server
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Fatal to listen and serve: %v\n", err)
+		var wg sync.WaitGroup
+		// 优雅关闭
+		exit := make(chan os.Signal)
+		//监听 Ctrl+C 信号
+		signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+		go func() {
+			<-exit
+			wg.Add(1)
+			defer wg.Done()
+			//使用context控制srv.Shutdown的超时时间
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			defer cancel()
+			if err := srv.Shutdown(ctx); err != nil {
+				log.Fatalf("Fatal to shutdown: %v\n", err)
+			}
+		}()
+
+		// 启动 server
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Fatal to listen and serve: %v\n", err)
+		}
+
+		wg.Wait()
+
+		log.Println("Gracefully shutdown the http server...")
+	*/
+
+	// 优雅停机：使用 endless 实现
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endPoint, r)
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d\n", syscall.Getpid())
 	}
 
-	wg.Wait()
-
-	log.Println("Gracefully shutdown the http server...")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server err: %v\n", err)
+	}
 }
